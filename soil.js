b88e1b5bf1e0f1e739390d5471d988b64c8211dd
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-02-11 17:37:38 -0500

    ph working

--- soil.js
+++ soil.js
@@ -2,11 +2,21 @@ let submit = document.getElementById('submitBtn')
 submit.onclick = callSoilData()
 
 async function callSoilData() {
-    let response = await fetch('https://rest.isric.org/soilgrids/v2.0/properties/query?lon=33&lat=33&property=nitrogen&property=phh2o&depth=0-5cm&value=Q0.5')
-    let data = await response.json()
-    console.log(data)
-    let nitrogen = data.layers[4]
-    let ph = data.layers[7]
+    let zip_url = await fetch('https://forecast.weather.gov/zipcity.php?inputstring=30318')
 
+    
+    let lat = parseInt(zip_url['url'].substring(zip_url['url'].length - 20, zip_url['url'].length - 13))
+    let lon = parseInt(zip_url['url'].substring(zip_url['url'].length - 8))
+
+    let url = `https://rest.isric.org/soilgrids/v2.0/properties/query?lon=${lon}&lat=${lat}&property=nitrogen&property=phh2o&depth=0-5cm&value=Q0.5`
+    let response = await fetch(url)
+    let data = await response.json()
+
+    let nitrogen = await data['properties']['layers'][0]['depths'][0]['values']['Q0.5']
+
+    let ph_temp = await data['properties']['layers'][1]['depths'][0]['values']['Q0.5']
+    let ph = parseInt(ph_temp) / 10
+
+    console.log(ph)
     return response
 }
