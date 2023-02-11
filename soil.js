c9cfb0bfb669636c851fa6da72104861eb9db584
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-02-10 23:53:52 -0500

    successful api call

--- soil.js
+++ soil.js
@@ -1,4 +1,9 @@
 let submit = document.getElementById('submitBtn')
-submit.onclick = function() {
-    alert('test')
+submit.onclick = callSoilData()
+
+async function callSoilData() {
+    let response = await fetch('https://rest.isric.org/soilgrids/v2.0/properties/layers')
+    let data = await response.json()
+    console.log(data)
+    return response
 }
