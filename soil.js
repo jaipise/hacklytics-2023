4a545c48073d5d0e14f524fa98ac440dcbe8baf7
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-02-11 16:01:35 -0500

    styles

--- soil.js
+++ soil.js
@@ -2,8 +2,11 @@ let submit = document.getElementById('submitBtn')
 submit.onclick = callSoilData()
 
 async function callSoilData() {
-    let response = await fetch('https://rest.isric.org/soilgrids/v2.0/properties/layers')
+    let response = await fetch('https://rest.isric.org/soilgrids/v2.0/properties/query?lon=33&lat=33&property=nitrogen&property=phh2o&depth=0-5cm&value=Q0.5')
     let data = await response.json()
     console.log(data)
+    let nitrogen = data.layers[4]
+    let ph = data.layers[7]
+
     return response
 }
